7cd7663c24f804560bbfe0cc020eb534ac0d17ef
ваше имя <84718361+Miros908@users.noreply.github.com>
2021-08-25 21:12:10 +0300

    Автоматизация сборки

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,22 @@
+let button = document.querySelector(".main-nav__toggle");
+let header = document.querySelector(".header");
+let list = document.querySelector(".main-nav__list");
+
+button.classList.remove("main-nav__toggle--close");
+button.classList.add("main-nav__toggle--open");
+header.classList.remove("header__open-menu");
+list.classList.remove("main-nav__list--open");
+
+button.addEventListener("click", function() {
+    list.classList.toggle("main-nav__list--open");
+    header.classList.toggle("header__open-menu");
+    button.classList.toggle("main-nav__toggle--open");
+    button.classList.toggle("main-nav__toggle--close");
+
+})
+
+
+//button
 let button1 = document.querySelector(".comments__button-1");
 let button2 = document.querySelector(".comments__button-2");
 let button3 = document.querySelector(".comments__button-3");
